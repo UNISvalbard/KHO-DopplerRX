e97d882f5b2e19b6209de37697ed2d8bf78fac76
mikkosyrjasuo <60669066+mikkosyrjasuo@users.noreply.github.com>
2024-01-04 16:09:37 +0100

    Update inspect_npz.py
    Show the time coverage for each file

--- DataHandling/inspect_npz.py
+++ DataHandling/inspect_npz.py
@@ -7,16 +7,26 @@ Essentially prints out the array names and the array sizes in each npz-file
 
 import sys
 import numpy as np
+import datetime as dt
+import pytz
 
+utctz=pytz.timezone('UTC')
 if len(sys.argv) == 1:
     print("inspect_npz.py {filename.npz} [filename2.npz] [...]")
     sys.exit()
+    
 for thisfile in sys.argv[1:]:
     print(thisfile)
     try:
         a = np.load(thisfile)
         for i in a.files:
             print("\t", i, a[i].shape)
+        ts_min=np.min(a['timestamps'])
+        ts_max=np.max(a['timestamps'])
+        data_start=dt.datetime.fromtimestamp(ts_min,tz=utctz)
+        data_end=dt.datetime.fromtimestamp(ts_max,tz=utctz)
+        print('\tFrom:',data_start)
+        print('\tTo:  ',data_end)
     except FileNotFoundError:
         print("\tFile not found!")
     except ValueError:
