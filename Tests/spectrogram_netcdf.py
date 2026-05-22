adf7b716ef11fa626da8cfe8bfbd84d08213add8
OlivierGdM <OlivierGdM@users.noreply.github.com>
2026-05-22 10:51:10 +0200

    Added netcdf file saver

--- Tests/spectrogram_netcdf.py
+++ Tests/spectrogram_netcdf.py
@@ -3,6 +3,8 @@ Created on Thu May 21
 
 @author: Olivier
 
+This script is based on existing code found in spectrogram_1h.py
+
 This script takes in a raw 24h PRIDE netcdf file and creates a spectrogram.
 The spectrogram is saved as a time series in a netcdf file.
 The spectrogram can also be plotted by the script using flag
@@ -28,19 +30,20 @@ args = parser.parse_args()
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-filename = Path(config['spectrogram_netcdf-settings']['file_to_read'])
+raw_data_file = Path(config['spectrogram_netcdf-settings']['raw_data_file'])
+destination_folder = Path(config['spectrogram_netcdf-settings']['destination_folder'])
 
 
 # ------------------------------------------------
 # Read 24h netcdf data file
 
-raw_dataset = xr.open_dataset(filename, decode_cf=False)
+raw_dataset = xr.open_dataset(raw_data_file, decode_cf=False)
 
 ms_since_start = np.array(raw_dataset['time'])
 start_date_str = raw_dataset['time'].attrs['units'].split(' ')[2]
-start_timestamp = dt.datetime.fromisoformat(start_date_str).timestamp()
+start_date = dt.datetime.fromisoformat(start_date_str)
 
-timestamps = ms_since_start/1000 + start_timestamp
+timestamps = ms_since_start/1000 + start_date.timestamp()
 samples_IQ = np.array(raw_dataset['samples_I'] + raw_dataset['samples_Q'] * 1j)
 
 order = np.argsort(timestamps)
@@ -64,4 +67,49 @@ max_freq = f[max_index]
 
 # # Convert spectrogram time to minutes
 # time_minutes = t / 60
+
+
+# ------------------------------------------------
+# Create the dataset, add metadata and save it to netcdf file
+
+prideds = xr.Dataset()
+
+# Time series
+prideds = xr.Dataset(coords={'time': ms_since_start})
+prideds['max_freq'] = ("time", max_freq)
+
+# Metadata
+prideds['time'].attrs = {
+    'standard_name':'time',
+    'long_name': 'time',
+    'units': f'milliseconds since {start_date.strftime('%Y-%m-%dT%H:%M:%SZ')}',
+    'calendar': 'standard',
+    'coverage_content_type': 'coordinate'
+}
+
+prideds['max_freq'].attrs = {
+    'long_name': 'Maximum frequency',
+    'units': 'Hz',
+    'coverage_content_type': 'physicalMeasurement'
+}
+
+# Export to netcdf
+time_series_file = destination_folder / f'test_PRIDE_spectrogram_{start_date.strftime('%Y%m%d')}.nc'
+
+encoding = {
+    'time': {
+        'dtype': 'int64',
+        '_FillValue': None  # Coordinate variables should not have fill values.
+    },
+    'max_freq': {
+        'dtype': 'float64',
+        '_FillValue': None
+    },
+}
+
+prideds.to_netcdf(time_series_file, encoding=encoding)
+
+
+# ------------------------------------------------
+# Plot the obtained data
 
